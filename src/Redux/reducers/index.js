191e86832df642de75531f9c0b8d825a09d3dfc2
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-23 18:05:34 +0600

    Add currentProjectBugs, currentRoute and navTitle reducer

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -3,12 +3,18 @@ import userReducer from './user';
 import projectsReducer from './projects';
 import bugsReducer from './bugs';
 import currentBugReducer from './currentBug';
+import currentProjectBugsReducer from './currentProjectBugs';
+import currentRouteReducer from './currentRoute';
+import navTitleReducer from './navTitle';
 
 const rootReducer = combineReducers({
   user: userReducer,
   projects: projectsReducer,
   bugs: bugsReducer,
+  currentProjectBugs: currentProjectBugsReducer,
   currentBugReport: currentBugReducer,
+  currentRoute: currentRouteReducer,
+  navTitle: navTitleReducer,
 });
 
 export default rootReducer;
