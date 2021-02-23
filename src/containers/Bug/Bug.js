9f7117d18890d99a2980b3e94c3485f2581d828d
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-23 17:55:47 +0600

    Add bug controll features

--- src/containers/Bug/Bug.js
+++ src/containers/Bug/Bug.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { fetchBugReport, createComment } from '../../Redux/actions/bugs';
+import {
+  fetchBugReport, createComment, assignBug, resolveBug, cancelResolve, cancelAssign,
+} from '../../Redux/actions/bugs';
 import styles from './Bug.module.css';
+import send from '../../assets/send.svg';
 
 const Bug = ({
-  currentBugReport, createComment, match, user, fetchBugReport,
+  currentBugReport, createComment, match, user,
+  fetchBugReport, assignBug, resolveBug, cancelResolve, cancelAssign,
 }) => {
   useEffect(() => {
     fetchBugReport(user.token, parseInt(match.params.id, 10));
@@ -27,6 +31,38 @@ const Bug = ({
     });
   };
 
+  const handleAssign = () => {
+    const assign = {
+      user_id: user.user.id,
+      bug_id: currentBugReport.data.bug.id,
+    };
+    assignBug(user.token, assign);
+  };
+
+  const handleCancelAssign = () => {
+    const assign = {
+      user_id: user.user.id,
+      bug_id: currentBugReport.data.bug.id,
+    };
+    cancelAssign(user.token, assign);
+  };
+
+  const handleResolve = () => {
+    const resolve = {
+      user_id: user.user.id,
+      bug_id: currentBugReport.data.bug.id,
+    };
+    resolveBug(user.token, resolve);
+  };
+
+  const handleCancelResolve = () => {
+    const resolve = {
+      user_id: user.user.id,
+      bug_id: currentBugReport.data.bug.id,
+    };
+    cancelResolve(user.token, resolve);
+  };
+
   const handleSubmit = () => {
     createComment(user.token, state);
   };
@@ -35,24 +71,101 @@ const Bug = ({
     return <div>Loading bug report...</div>;
   }
 
+  let bugControlls;
+
+  if (currentBugReport.data.bug.status === 'open') {
+    bugControlls = (
+      <div>
+        <button className={styles['yes-btn']} type="button" onClick={handleAssign}>Assign to me</button>
+      </div>
+    );
+  } else if (currentBugReport.data.bug.status === 'assigned') {
+    if (currentBugReport.data.assign.user_id === user.user.id) {
+      bugControlls = (
+        <div>
+          <p className={styles['font-style']}>
+            <b>Assined to:</b>
+            {currentBugReport.data.assignee_name}
+          </p>
+          <button className={styles['cancel-btn']} type="button" onClick={handleCancelAssign}>I don&apos;t want to resolve the bug</button>
+          <button className={styles['yes-btn']} type="button" onClick={handleResolve}>I have resolved the bug</button>
+        </div>
+      );
+    } else {
+      bugControlls = (
+        <div>
+          <p className={styles['font-style']}>
+            <b>Assigned to:</b>
+            {currentBugReport.data.assignee_name}
+          </p>
+        </div>
+      );
+    }
+  } else if (currentBugReport.data.bug.status === 'resolved') {
+    if (currentBugReport.data.assign.user_id === user.user.id) {
+      bugControlls = (
+        <div>
+          <p className={styles['font-style']}>
+            <b>Resolved by:</b>
+            {currentBugReport.data.assignee_name}
+          </p>
+          <button className={styles['cancel-btn']} type="button" onClick={handleCancelResolve}>I didn&apos;t resolve the bug</button>
+        </div>
+      );
+    } else {
+      bugControlls = (
+        <div>
+          <p className={styles['font-style']}>
+            <b>Resolved by:</b>
+            {currentBugReport.data.assignee_name}
+          </p>
+        </div>
+      );
+    }
+  }
+
   return (
     <>
-      <div>
-        <p>{currentBugReport.data.bug.title}</p>
-        <p>{currentBugReport.data.bug.description}</p>
+      <div className={styles.details}>
+        <p className={styles['font-style-title']}>{currentBugReport.data.bug.title}</p>
+        <p className={styles['font-style']}>
+          <b>Description:</b>
+          <br />
+          {currentBugReport.data.bug.description}
+        </p>
+        <p className={styles['font-style']}>
+          <b>Author:</b>
+          {currentBugReport.data.author_name}
+        </p>
+        <p className={styles['font-style']}>
+          <b>Status:</b>
+          {currentBugReport.data.bug.status}
+        </p>
+        {bugControlls}
       </div>
-      <div>
-        <form>
-          <label htmlFor="comment">
-            <p>Write a comment</p>
-            <input type="text" id="content" name="content" required onChange={handleChange} />
-          </label>
-          <button type="button" onClick={handleSubmit}>Submit</button>
-        </form>
-      </div>
-      <div>
-        <p className={styles.ray}>Comments</p>
-        {currentBugReport.data.comments.map(item => <p key={item.id}>{item.content}</p>)}
+      <div className={styles['comments-container']}>
+        <div>
+          <p className={styles['comment-header']}>Comments</p>
+          <form className={styles['comment-form']}>
+            <textarea placeholder="Write your comment here.." type="text" id="content" name="content" required onChange={handleChange} />
+            <button type="button" onClick={handleSubmit}>
+              <img className={styles.send} src={send} alt="send" />
+            </button>
+          </form>
+        </div>
+        <div>
+          {currentBugReport.data.comments.map(
+            item => (
+              <div key={item.id} className={styles.comment}>
+                <p>{item.content}</p>
+                <p>
+                  Author:
+                  {currentBugReport.data.users.filter(user => user.id === item.user_id)[0].username}
+                </p>
+              </div>
+            ),
+          )}
+        </div>
       </div>
     </>
   );
@@ -64,6 +177,10 @@ Bug.propTypes = {
   currentBugReport: PropTypes.instanceOf(Object).isRequired,
   fetchBugReport: PropTypes.func.isRequired,
   createComment: PropTypes.func.isRequired,
+  assignBug: PropTypes.func.isRequired,
+  resolveBug: PropTypes.func.isRequired,
+  cancelResolve: PropTypes.func.isRequired,
+  cancelAssign: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -72,5 +189,7 @@ const mapStateToProps = state => ({
   currentBugReport: state.currentBugReport,
 });
 
-const mapDispatchToProps = { fetchBugReport, createComment };
+const mapDispatchToProps = {
+  fetchBugReport, createComment, assignBug, resolveBug, cancelResolve, cancelAssign,
+};
 export default connect(mapStateToProps, mapDispatchToProps)(Bug);
