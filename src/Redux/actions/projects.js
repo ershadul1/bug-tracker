e11f6219d7a8adc8954f7c82dc1e9b28191b617e
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-23 18:01:11 +0600

    Remove console statement

--- src/Redux/actions/projects.js
+++ src/Redux/actions/projects.js
@@ -1,60 +1,54 @@
 import URL from './url';
 
 const fetchProjects = token => dispatch => {
-  const pass = token || localStorage.getItem('bug-tracker');
   fetch(`${URL}/projects/all`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${pass}`,
+      Authorization: `Bearer ${token}`,
     },
   })
     .then(response => response.json())
     .then(data => {
-      console.log('Success:', data);
       dispatch({ type: 'FETCH_PROJECTS', payload: data });
     })
     .catch(error => {
-      console.error('Error:', error);
+      throw new Error('Error:', error);
     });
 };
 
 const fetchProjectBugs = (token, id) => dispatch => {
-  const pass = token || localStorage.getItem('bug-tracker');
   fetch(`${URL}/projects?id=${id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${pass}`,
+      Authorization: `Bearer ${token}`,
     },
   })
     .then(response => response.json())
     .then(data => {
-      console.log('Success:', data);
       dispatch({ type: 'FETCH_PROJECT_BUGS', payload: data });
     })
     .catch(error => {
-      console.error('Error:', error);
+      throw new Error('Error:', error);
     });
 };
 
 const createProject = (token, data) => dispatch => {
-  const pass = token || localStorage.getItem('bug-tracker');
   fetch(`${URL}/projects`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${pass}`,
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(data),
   })
     .then(response => response.json())
     .then(data => {
-      console.log('Success:', data);
       dispatch({ type: 'CREATE_PROJECT', payload: data });
     })
     .catch(error => {
-      console.error('Error:', error);
+      throw new Error('Error:', error);
     });
 };
 
