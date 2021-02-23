422e6b0c0529e7b9f4210fb5693e2b0b6bb45f70
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-23 18:04:44 +0600

    Get token from localstorage in initial state

--- src/Redux/reducers/user.js
+++ src/Redux/reducers/user.js
@@ -1,13 +1,21 @@
-const userReducer = (state = {}, action) => {
+const initialState = {
+  user: { username: '' },
+  token: localStorage.getItem('bug-tracker') || null,
+};
+
+const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'LOGIN': {
-      return { ...action.payload };
+      return { ...state, ...action.payload };
     }
     case 'AUTO_LOGIN': {
-      return { user: { ...action.payload } };
+      return { ...state, user: { ...action.payload } };
     }
     case 'SIGNUP': {
-      return { ...action.payload };
+      return { ...state, ...action.payload };
+    }
+    case 'LOGOUT': {
+      return {};
     }
     default:
       return state;
