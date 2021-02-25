5d055025a9fceee6bc8aa8ff0c45af4fa0146eb0
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-25 18:28:50 +0600

    Restructure routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { autoLogin } from './Redux/actions/users';
+import { getUser } from './Redux/actions/users';
 import Login from './containers/Login/Login';
 import SignUp from './containers/Signup/SignUp';
 import ProjectList from './containers/ProjectList/ProjectList';
@@ -15,17 +15,14 @@ import BugList from './containers/BugList/BugList';
 import TopNav from './containers/TopNav/TopNav';
 import More from './containers/More/More';
 
-const Routes = ({ user, autoLogin }) => {
-  useEffect(() => {
-    autoLogin(user.token);
-  }, []);
-
+const Routes = ({ user, getUser, error }) => {
   const loggedIn = !!user.token;
 
   if (!loggedIn) {
     return (
       <BrowserRouter>
         <TopNav />
+        <div className="error">{error}</div>
         <div className="app">
           <Switch>
             <Route path="/signup" exact component={SignUp} />
@@ -36,18 +33,22 @@ const Routes = ({ user, autoLogin }) => {
     );
   }
 
+  useEffect(() => {
+    getUser(user.token);
+  }, []);
+
   return (
     <BrowserRouter>
       <TopNav />
       <div className="app">
         <Switch>
           <Route path="/more" exact component={More} />
-          <Route path="/projects/:id" exact component={ProjectBugs} />
-          <Route path="/new/project" exact component={CreateProject} />
+          <Route path="/projects/new" exact component={CreateProject} />
+          <Route path="/projects/:project_id" exact component={ProjectBugs} />
           <Route path="/projects" exact component={ProjectList} />
           <Route path="/bugs" exact component={BugList} />
-          <Route path="/bugs/:id" exact component={Bug} />
-          <Route path="/new/bug" exact component={CreateBugReport} />
+          <Route path="/projects/:project_id/bugs/:bug_id" exact component={Bug} />
+          <Route path="/bugs/new" exact component={CreateBugReport} />
           <Route path="/" exact component={ProjectList} />
         </Switch>
       </div>
@@ -58,10 +59,11 @@ const Routes = ({ user, autoLogin }) => {
 
 Routes.propTypes = {
   user: PropTypes.instanceOf(Object).isRequired,
-  autoLogin: PropTypes.func.isRequired,
+  getUser: PropTypes.func.isRequired,
+  error: PropTypes.string.isRequired,
 };
 
-const mapStateToProps = state => ({ user: state.user });
+const mapStateToProps = state => ({ user: state.user, error: state.error });
 
-const mapDispatchToProps = { autoLogin };
+const mapDispatchToProps = { getUser };
 export default connect(mapStateToProps, mapDispatchToProps)(Routes);
