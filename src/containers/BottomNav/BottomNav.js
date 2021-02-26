88ad3c7259b17eaa8ba95a6eaed588efd3c38192
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-26 13:35:15 +0600

    Implement useLocation in BottomNav

--- src/containers/BottomNav/BottomNav.js
+++ src/containers/BottomNav/BottomNav.js
@@ -1,48 +1,40 @@
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './BottomNav.module.css';
 import add from '../../assets/add.svg';
 import bug from '../../assets/bug.svg';
 import project from '../../assets/project.svg';
 import more from '../../assets/more.svg';
 
-const BottomNav = ({ currentRoute }) => (
-  <>
-    <div className={styles.container}>
-      <Link to="/bugs/new" className={currentRoute === '/bugs/new' ? styles.selected : null}>
-        <img src={add} alt="add-bug" className={styles.icon} />
-        <div>Add Bug Report</div>
-      </Link>
-      <Link
-        to="/projects"
-        className={(currentRoute === '/projects' ? styles.selected : null)
-      || (currentRoute === '/' ? styles.selected : null)}
-      >
-        <img src={project} alt="projects" className={styles.icon} />
-        <div>Projects</div>
-      </Link>
-      <Link to="/bugs" className={currentRoute === '/bugs' ? styles.selected : null}>
-        <img src={bug} alt="bugs" className={styles.icon} />
-        <div>Bugs</div>
-      </Link>
-      <Link to="/more" className={currentRoute === '/more' ? styles.selected : null}>
-        <img src={more} alt="more" className={styles.icon} />
-        <div>More</div>
-      </Link>
-    </div>
-  </>
-);
+const BottomNav = () => {
+  const location = useLocation();
+  const currentRoute = location.pathname;
 
-BottomNav.propTypes = {
-  currentRoute: PropTypes.string.isRequired,
+  return (
+    <>
+      <div className={styles.container}>
+        <Link to="/bugs/new" className={currentRoute === '/bugs/new' ? styles.selected : null}>
+          <img src={add} alt="add-bug" className={styles.icon} />
+          <div>Add Bug Report</div>
+        </Link>
+        <Link
+          to="/projects"
+          className={(currentRoute === '/projects' ? styles.selected : null)
+      || (currentRoute === '/' ? styles.selected : null)}
+        >
+          <img src={project} alt="projects" className={styles.icon} />
+          <div>Projects</div>
+        </Link>
+        <Link to="/bugs" className={currentRoute === '/bugs' ? styles.selected : null}>
+          <img src={bug} alt="bugs" className={styles.icon} />
+          <div>Bugs</div>
+        </Link>
+        <Link to="/more" className={currentRoute === '/more' ? styles.selected : null}>
+          <img src={more} alt="more" className={styles.icon} />
+          <div>More</div>
+        </Link>
+      </div>
+    </>
+  );
 };
 
-const mapStateToProps = state => ({
-  user: state.user,
-  projects: state.projects,
-  currentBugReport: state.currentBugReport,
-  currentRoute: state.currentRoute,
-});
-
-export default connect(mapStateToProps, null)(BottomNav);
+export default BottomNav;
